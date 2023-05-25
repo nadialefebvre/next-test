9d9746da955c032a87ec618602ce87e96cc7ee71
Nadia Lefebvre <95084122+nadialefebvre@users.noreply.github.com>
2023-05-25 16:38:03 +0200

    add head and layout

--- pages/posts/first-post.tsx
+++ pages/posts/first-post.tsx
@@ -1,13 +1,16 @@
-import Link from 'next/link';
+import Link from "next/link"
+import Head from "next/head"
+import Layout from "../../components/layout"
 
 const FirstPost = () => {
   return (
-    <>
+    <Layout>
+      <Head>
+        <title>This is a first post</title>
+      </Head>
       <h1>First Post</h1>
-      <h2>
-        <Link href="/">Back to home</Link>
-      </h2>
-    </>
-  );}
+    </Layout>
+  )
+}
 
 export default FirstPost
